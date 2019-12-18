10deb06b05114d3abb1100694fd54259b0deae60
karthikpobbathi <35491482+karthikpobbathi@users.noreply.github.com>
2019-12-18 10:46:34 +0530

    Add files via upload

---
+++ Simple2/src/mypackage3/ModifyName.java
@@ -0,0 +1,19 @@
+package mypackage3;
+
+import myPackage.MyClass;
+
+public class ModifyName {
+public static void main(String[] args) {
+	{       
+	      // Initializing the String variable 
+	      // with a value 
+	      String name = "GeeksforGeeks";
+	      
+	      // Creating an instance of class MyClass in 
+	      // the package.
+	      MyClass obj = new MyClass();
+	      
+	      obj.getNames(name);
+	   }
+}
+}
